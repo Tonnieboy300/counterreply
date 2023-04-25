6fa1dd38e5514b2ec66c56dad0089a85d029363c
Tonnieboy300 <tonnieboy300@icloud.com>
2023-04-24 17:29:21 -0700

    set up a basic question screen

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-localStorage.setItem("text", "click it");
+var currentQuestionNum = 1
 
-document.getElementById("test").innerHTML = localStorage.getItem("text");
 
-doit = () => {
-    localStorage.setItem("text", ":)");
-    document.getElementById("test").innerHTML = localStorage.getItem("text");
+
+begin = () => {
+    localStorage.setItem("question1", "");
+    window.location.href = "./question1.html";
+}
+
+question = () => {
+    localStorage.setItem("question" + currentQuestionNum,document.getElementById("questionbox").value);
+    document.getElementById("question1").innerHTML = localStorage.getItem("question" + currentQuestionNum);
+    document.getElementById("questionbox").value = "";
 }

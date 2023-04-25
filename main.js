37a32802ca9082e7b186c495f4dcd11293ed8317
Tonnieboy300 <tonnieboy300@icloud.com>
2023-04-24 17:52:37 -0700

    create answer input and display pages

--- main.js
+++ main.js
@@ -3,12 +3,38 @@ var currentQuestionNum = 1
 
 
 begin = () => {
-    localStorage.setItem("question1", "");
-    window.location.href = "./question1.html";
+    localStorage.clear();
+    window.location.href = "./question.html";
 }
 
-question = () => {
+questionStore = () => {
     localStorage.setItem("question" + currentQuestionNum,document.getElementById("questionbox").value);
-    document.getElementById("question1").innerHTML = localStorage.getItem("question" + currentQuestionNum);
-    document.getElementById("questionbox").value = "";
+    window.location.href = "./answerInput.html";
+}
+
+loadQuestion = () => {
+    document.getElementById("askQuestion").innerHTML = localStorage.getItem("question" + currentQuestionNum);
+}
+
+answerSubmit = (answerNum) => {
+    localStorage.setItem("answer" + answerNum, document.getElementById("answer" + answerNum).value)
+    //checks if both have been submitted
+    if (answerNum == "One"){
+        localStorage.setItem("answerOneDone", "yes");
+    }
+    if (answerNum == "Two"){
+        localStorage.setItem("answerTwoDone", "yes");
+    }
+
+    if (localStorage.getItem("answerOneDone") == "yes" && localStorage.getItem("answerTwoDone") == "yes"){
+        localStorage.setItem("answerOneDone", "no");
+        localStorage.setItem("answerTwoDone", "no");
+        window.location.href = "./displayAnswers.html";
+    }
+}
+
+loadAnswers = () => {
+    loadQuestion();
+    document.getElementById("answer1").innerHTML = localStorage.getItem("answerOne")
+    document.getElementById("answer2").innerHTML = localStorage.getItem("answerTwo")
 }
